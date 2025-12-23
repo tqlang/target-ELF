1a43229a0f97f8c0a51b7abbe2fd8deefc8cee00
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-12-23 00:33:32 -0300

    a

--- Tq.Module.CLR/Module.cs
+++ Tq.Module.CLR/Module.cs
@@ -1,16 +1,15 @@
-﻿using Tq.Module.LLVM.Targets;
-using Tq.Realizer.Core.Configuration;
+﻿using Tq.Realizer.Core.Configuration;
 using Tq.Realizer.Core.Configuration.LangOutput;
 
-namespace Tq.Module.CLR;
+namespace Tq.Module.ELF;
 
 
 public class Module: IModule
 {
     public ModuleConfiguration Config { get; } = new()
     {
-        Name = "CLR",
-        Description = "Provides targets for compiling to microsoft's Common Language Runtime assemblies",
+        Name = "ELF",
+        Description = "Provides targets for compiling to realizer's executable and linkable format",
         
         Author = "lumi2021",
         Version = "1.0.0",
@@ -18,16 +17,16 @@ public class Module: IModule
         Targets = [
             new TargetConfiguration
             {
-                TargetName = ".NET CLR",
-                TargetDescription = "Compiles to microsoft's Common Language Runtime assemblies",
-                TargetIdentifier = "dotnet",
+                TargetName = "realizer's ELF",
+                TargetDescription = "Compiles to realizer's executable and linkable format",
+                TargetIdentifier = "elf",
                 
                 LanguageOutput = new OmegaOutputConfiguration() {
-                    BakeGenerics = true,
-                    AbstractingOptions = AbstractingOptions.NoNamespaces,
+                    BakeGenerics = false,
+                    AbstractingOptions = AbstractingOptions.None,
                     
-                    MemoryUnit = 8,
-                    NativeIntegerSize = 32,
+                    MemoryUnit = 1,
+                    NativeIntegerSize = 0,
                     
                     GenericAllowedFeatures = GenericAllowedFeatures.None, 
                     OmegaAllowedFeatures = OmegaAllowedFeatures.None,
