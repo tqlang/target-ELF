4573e091f7778ff419deca4a0f10175d61dbefd6
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-12-23 00:39:14 -0300

    minnor changes

---
+++ Tq.Module.ELF/Compiler.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Reflection.Emit;
+using Tq.Realizer.Core.Configuration.LangOutput;
+using Tq.Realizer.Core.Program;
+using Tq.Realizer.Core.Program.Builder;
+using Tq.Realizer.Core.Program.Member;
+
+namespace Tq.Module.ELF;
+
+public class Compiler
+{
+   
+    public void Compile(RealizerProgram program, IOutputConfiguration config)
+    {
+        Console.WriteLine("Serializing to ELF...");
+        Console.WriteLine("TODO");
+
+    }
+    
+}
---
+++ Tq.Module.ELF/Module.cs
@@ -0,0 +1,39 @@
+﻿using Tq.Realizer.Core.Configuration;
+using Tq.Realizer.Core.Configuration.LangOutput;
+
+namespace Tq.Module.ELF;
+
+
+public class Module: IModule
+{
+    public ModuleConfiguration Config { get; } = new()
+    {
+        Name = "ELF",
+        Description = "Provides targets for compiling to realizer's executable and linkable format",
+        
+        Author = "lumi2021",
+        Version = "1.0.0",
+        
+        Targets = [
+            new TargetConfiguration
+            {
+                TargetName = "realizer's ELF",
+                TargetDescription = "Compiles to realizer's executable and linkable format",
+                TargetIdentifier = "elf",
+                
+                LanguageOutput = new OmegaOutputConfiguration() {
+                    BakeGenerics = false,
+                    AbstractingOptions = AbstractingOptions.None,
+                    
+                    MemoryUnit = 1,
+                    NativeIntegerSize = 0,
+                    
+                    GenericAllowedFeatures = GenericAllowedFeatures.None, 
+                    OmegaAllowedFeatures = OmegaAllowedFeatures.None,
+                },
+                
+                CompilerInvoke = (a, b) => new Compiler().Compile(a, b)
+            },
+        ]
+    };
+}
